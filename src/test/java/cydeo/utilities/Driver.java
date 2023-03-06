80a559486fc3f017e3e764dcee11783d24cc8a1a
Mustafa Atik <112565940+MustafaAtik76@users.noreply.github.com>
2023-03-06 21:40:48 +0300

    Update Driver.java

--- src/test/java/cydeo/utilities/Driver.java
+++ src/test/java/cydeo/utilities/Driver.java
@@ -46,6 +46,18 @@ public class Driver {
                 switch statement will determine the case, and open the matching browser
             */
             switch (browserType){
+                         case "remote-chrome":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "54.160.69.233";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
                 case "chrome":
 
                     WebDriverManager.chromedriver().setup();
